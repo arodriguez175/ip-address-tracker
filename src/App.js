94c38a2bd48c1cab7bbc7ac73497a6639d550c11
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-05-11 18:01:04 -0400

    feat: add HTML tags to act as a basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>IP Address Tracker</h1>
+      <input placeholder="Search for any IP address or domain" />
+      <button></button>
+      <div>
+        <p>IP ADDRESS</p>
+        {/* IP address */}
+
+        <p>LOCATION</p>
+        {/* City, State Zip */}
+
+        <p>TIMEZONE</p>
+        {/* Current UTC time */}
+
+        <p>ISP</p>
+        {/* ISP name */}
+      </div>
     </div>
   );
 }
